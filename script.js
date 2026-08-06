d1f5a1cf8adf058f149e8fbb6db76bb4f3f35b62
vparra9 <victoria.j.parra@outlook.com>
2026-08-06 18:33:08 +0000

    Established frontend to send data over to python (backend)

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Grab HTML elements
 const logInput = document.getElementById('logInput');
 const uploadText = document.getElementById('uploadText');
+const analyzeButton = document.getElementById('analyzeBtn');
+
+// Global container to hold the file so both functions have access
+let uploadedFile = null;
+/****Handles file that grabs log file *****/
 
 // When user selects file
 logInput.addEventListener('change', function(){
@@ -22,10 +27,49 @@ logInput.addEventListener('change', function(){
     // If file size is fine, filename is displayed
     uploadText.innerText = `${file.name}`;
     uploadText.style.color = "#28a745";
+
+    // Saves valid file to global container
+    uploadedFile = file;
+
     }
 });
 
-/* Shows user it grabbed file*/
+/****Handle button that analyzes log files uploaded ****/
 
+// Tells browser to listen for a click event
+analyzeButton.addEventListener('click', function() {
 
-/* Checks if over 5mb and blocks if it is*/
+    // Alerts user if they click analyze without uploading a file
+    if(!uploadedFile) {
+        alert("Please upload a valid log file.")
+    return;
+    }
+
+    console.log("File is being prepared for analysis: ", uploadedFile.name);
+
+    const formData = new FormData();
+    formData.append('log_file', uploadedFile);
+
+    fetch('https://miniature-umbrella-97q46qw79jpw39vgx-5000.app.github.dev/upload/', { 
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Python's summary results will return
+        console.log("Analysis results from Python:", data);
+
+        // Displays newly calculated backend results
+        alert(`Analysis done for ${data.filename}
+                -Errors: ${data.errors}
+                -Failed Logins: ${data.failed_logins}
+                -Successful Logins: ${data.successful_logins}
+                -File Changes: ${data.file_changes}`);
+
+    })
+    .catch(error => {
+        console.error("Connection failed:", error);
+        alert("Cannot connect to Python. Is your server running?");
+    });
+});
+
