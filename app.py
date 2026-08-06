3f2f0b2bae623a4c8439a4414ccc6a8dccf25cd6
vparra9 <victoria.j.parra@outlook.com>
2026-08-06 18:59:33 +0000

    Supported format section added, informing user of what files types are allowed and the maximum size prohibited.

--- app.py
+++ app.py
@@ -57,9 +57,10 @@ async def catch_file(log_file: UploadFile = File(...)):
         "filename": log_file.filename,
         "failed_logins": failed_logins,
         "successful_logins": successful_logins,
-        "erors": errors
+        "errors": errors,
+        "file_changes": file_changes
     }
 
 # Launch server
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
